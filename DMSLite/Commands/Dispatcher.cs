08d1eb1b72966c5f217e32b30bf8dd0905d7f8a7
pmiri <pmiriello@gmail.com>
2016-10-03 15:30:33 -0400

    Dispatcher structure created for #2

---
+++ DMSLite/Commands/Dispatcher.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ApiAiSDK;
+
+namespace DMSLite.Commands
+{
+    public class Dispatcher
+    {
+        private const string apiaikey = "9cc984ef80ef4502baa2de299ce11bbc"; //Client token used
+
+        private static ApiAi apiAi;
+
+        public Dispatcher()
+        {
+            InitAPIAI();
+
+        }
+
+        static void InitAPIAI()
+        {
+            var config = new AIConfiguration(apiaikey, SupportedLanguage.English);
+            apiAi = new ApiAi(config);
+        }
+
+        public void Dispatch(string request)
+        {
+            var response = apiAi.TextRequest(request);
+
+            Console.WriteLine(response.Result.Fulfillment.Speech);
+
+            // Get Client's Project Name
+            string project = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+
+            // Taken from API.ai returned JSON object
+            string action = response.Result.Action;
+
+            //Join strings to create classLocation
+            string classLocation = project + "." + action;
+
+            //Find the class as a Type
+            Type commandType = Type.GetType(classLocation);
+
+            //Check if null, if null return message to the UI
+            if (commandType == null) return;
+
+            //Cast and execute the command
+            iCommand command = Activator.CreateInstance(commandType) as iCommand;
+
+            try
+            {
+                //NOTE this returns a view
+                command.Execute(response.Result.Parameters);
+            }
+            catch (Exception e)
+            {
+                // Send Error message to the UI
+                return;
+            }
+        }
+    }
+}
---
+++ DMSLite/Commands/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace DMSLite.Commands
+{
+    public interface iCommand
+    {
+       View Execute(Dictionary<String, Object> parameters);
+    }
+}
