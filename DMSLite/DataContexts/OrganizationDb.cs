612dd16dac9e2100b603e48f48d3f5874946d8c6
Netopya <netopya@netopyaplanet.com>
2016-10-04 22:38:23 -0400

    Adding new class attempt
    -Attempted to create companies class for #2
    -Attempted to migrate entity changes to the database

---
+++ DMSLite/DataContexts/OrganizationDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DMSLite.Entities;
+using DMSLite.Models;
+
+namespace DMSLite.DataContexts
+{
+    public class OrganizationDb : DbContext
+    {
+        public OrganizationDb() : base("LocalMySqlServer")
+        {
+            Database.SetInitializer(new MySqlInitializer());
+        }
+
+        public DbSet<Organization> Organizations { get; set; }
+    }
+}
