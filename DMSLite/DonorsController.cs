51f295e310e2539cab67b7f10db4b9b589b8f88d
Netopya <netopya@netopyaplanet.com>
2016-10-07 14:34:07 -0400

    #2: Added donors model, controller, and views
    -Added donors and migrated it to the database
    -Scafolded a default controller and views

---
+++ DMSLite/DonorsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DMSLite.DataContexts;
+using DMSLite.Entities;
+
+namespace DMSLite
+{
+    public class DonorsController : Controller
+    {
+        private OrganizationDb db = new OrganizationDb();
+
+        // GET: Donors
+        public ActionResult Index()
+        {
+            return View(db.Donors.ToList());
+        }
+
+        // GET: Donors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donor donor = db.Donors.Find(id);
+            if (donor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donor);
+        }
+
+        // GET: Donors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,PhoneNumber,Type,ReceiptFrequency")] Donor donor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donors.Add(donor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donor);
+        }
+
+        // GET: Donors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donor donor = db.Donors.Find(id);
+            if (donor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donor);
+        }
+
+        // POST: Donors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,PhoneNumber,Type,ReceiptFrequency")] Donor donor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donor);
+        }
+
+        // GET: Donors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donor donor = db.Donors.Find(id);
+            if (donor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donor);
+        }
+
+        // POST: Donors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donor donor = db.Donors.Find(id);
+            db.Donors.Remove(donor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
