7082d74f0712683d1745b409d98f055bff4d663d
dsvoid <dsvoid@gmail.com>
2016-10-13 14:16:23 -0400

    #2: Added (some, not yet all) unit tests for viewing all donors and viewing specific donors

---
+++ DMSLite.Tests/Controllers/DonorsControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using DMSLite.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using DMSLite.Entities;
+using DMSLite.DataContexts;
+
+namespace DMSLite.Tests.Controllers
+{
+    [TestClass]
+    public class DonorsControllerTest
+    {
+        private OrganizationDb db = new OrganizationDb();
+
+        [TestMethod]
+        public void ViewDonors()
+        {
+            HomeController hc = new HomeController();
+            FormCollection fc = new FormCollection();
+            fc.Add("mainInput", "Show donors");
+            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
+            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
+
+            List<Donor> allDonors = db.Donors.ToList();
+
+            Assert.AreEqual(allDonors.Count(), returnedModel.Count());
+            Assert.AreEqual(allDonors[0].Name, returnedModel[0].Name);
+        }
+
+        [TestMethod]
+        public void ViewSpecificDonor()
+        {
+            HomeController hc = new HomeController();
+            FormCollection fc = new FormCollection();
+            fc.Add("mainInput", "Show me Denis");
+            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
+            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
+
+            List<Donor> denis = db.Donors.Where(x => x.Name == "Denis").ToList();
+            
+            Assert.AreEqual(denis.Count(), returnedModel.Count());
+            Assert.AreEqual(denis[0].Name, returnedModel[0].Name);
+        }
+    }
+}
