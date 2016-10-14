89d85c07053584acce1bc0756117dc5db4881ac7
dsvoid <dsvoid@gmail.com>
2016-10-14 14:10:09 -0400

    Tests for donor phone number and email

--- DMSLite.Tests/Controllers/DonorsControllerTest.cs
+++ DMSLite.Tests/Controllers/DonorsControllerTest.cs
@@ -36,14 +36,59 @@ namespace DMSLite.Tests.Controllers
         {
             HomeController hc = new HomeController();
             FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me Denis");
+            fc.Add("mainInput", "Show me Steve");
             PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
             var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
 
-            List<Donor> denis = db.Donors.Where(x => x.FirstName == "Denis").ToList();
+            List<Donor> steve = db.Donors.Where(x => x.FirstName == "Steve").ToList();
             
-            Assert.AreEqual(denis.Count(), returnedModel.Count());
-            Assert.AreEqual(denis[0].FirstName, returnedModel[0].FirstName);
+            Assert.AreEqual(steve.Count(), returnedModel.Count());
+            Assert.AreEqual(steve[0].FirstName, returnedModel[0].FirstName);
+        }
+
+        [TestMethod]
+        public void ViewMultipleDonors()
+        {
+            HomeController hc = new HomeController();
+            FormCollection fc = new FormCollection();
+            fc.Add("mainInput", "Show me Squidward");
+            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
+            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
+
+            List<Donor> squids = db.Donors.Where(x => x.FirstName == "Squidward").ToList();
+
+            Assert.AreEqual(squids.Count(), returnedModel.Count());
+            Assert.AreEqual(squids[0].FirstName, returnedModel[0].FirstName);
+        }
+
+        [TestMethod]
+        public void ViewByPhoneNumber()
+        {
+            HomeController hc = new HomeController();
+            FormCollection fc = new FormCollection();
+            fc.Add("mainInput", "Show me 555-555-5555");
+            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
+            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
+
+            List<Donor> phone = db.Donors.Where(x => x.PhoneNumber == "555-555-5555").ToList();
+
+            Assert.AreEqual(phone.Count(), returnedModel.Count());
+            Assert.AreEqual(phone[0].PhoneNumber, returnedModel[0].PhoneNumber);
+        }
+
+        [TestMethod]
+        public void ViewByEmail()
+        {
+            HomeController hc = new HomeController();
+            FormCollection fc = new FormCollection();
+            fc.Add("mainInput", "Show me steve@stevemail.com");
+            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
+            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
+
+            List<Donor> email = db.Donors.Where(x => x.Email == "steve@stevemail.com").ToList();
+
+            Assert.AreEqual(email.Count(), returnedModel.Count());
+            Assert.AreEqual(email[0].Email, returnedModel[0].Email);
         }
     }
 }
