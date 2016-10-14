e575af9fee8ff8ff11d4c179b615aea401f7abeb
denis.kefallinos <denis.kefallinos@silentpartnersoftware.com>
2016-10-14 14:18:54 -0400

    Unit test for searching for a donor with duplicate name using a last name. #2

--- DMSLite.Tests/Controllers/DonorsControllerTest.cs
+++ DMSLite.Tests/Controllers/DonorsControllerTest.cs
@@ -17,7 +17,7 @@ namespace DMSLite.Tests.Controllers
         private OrganizationDb db = new OrganizationDb();
 
         [TestMethod]
-        public void ViewDonors()
+        public void TestViewDonors()
         {
             HomeController hc = new HomeController();
             FormCollection fc = new FormCollection();
@@ -32,18 +32,35 @@ namespace DMSLite.Tests.Controllers
         }
 
         [TestMethod]
-        public void ViewSpecificDonor()
+        public void TestViewSpecificDonor()
         {
             HomeController hc = new HomeController();
             FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me Denis");
+            fc.Add("mainInput", "Show me Dionysios");
             PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
             var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
 
-            List<Donor> denis = db.Donors.Where(x => x.FirstName == "Denis").ToList();
+            List<Donor> denis = db.Donors.Where(x => x.FirstName == "Dionysios").ToList();
             
             Assert.AreEqual(denis.Count(), returnedModel.Count());
             Assert.AreEqual(denis[0].FirstName, returnedModel[0].FirstName);
+        }
+
+        [TestMethod]
+        //tests a case where a donor with a duplicate first name is searched for with their unique last name and only one proper donor is returned
+        public void TestViewSpecificDonorWithDuplicateName()
+        {
+            HomeController hc = new HomeController();
+            FormCollection fc = new FormCollection();
+            fc.Add("mainInput", "Show me Squidward Tentacles");
+            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
+            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
+
+            List<Donor> squidward = db.Donors.Where(x => x.FirstName == "Squidward" && x.LastName == "Tentacles").ToList();
+
+            Assert.AreEqual(squidward.Count(), 1);
+            Assert.AreEqual(squidward[0].FirstName, returnedModel[0].FirstName);
+            Assert.AreEqual(squidward[0].LastName, returnedModel[0].LastName);
         }
     }
 }
