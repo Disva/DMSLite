7208636e7c3f93397081c994fe197c6344120ffa
denis.kefallinos <denis.kefallinos@silentpartnersoftware.com>
2016-10-24 15:32:24 -0400

    Fixed all unit tests other than those related to adding donors. #3

--- DMSLite.Tests/Controllers/DonorsControllerTest.cs
+++ DMSLite.Tests/Controllers/DonorsControllerTest.cs
@@ -37,95 +37,101 @@ namespace DMSLite.Tests.Controllers
         //Tests that requesting a specific valid donor returns that valid donor.
         public void TestViewSpecificDonor()
         {
-            HomeController hc = new HomeController();
-            FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me Steve");
-            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
-            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
-
+            DonorsController dc = new DonorsController();
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("donor-search", new List<String> { "name", "Steve" });
+            parameters.Add("email-address", "");
+            parameters.Add("name", "Steve");
+            parameters.Add("phone-number", "");
+            PartialViewResult pvr = (PartialViewResult)dc.FetchDonor(parameters);
+            List<Donor> returnedModel = ((List<Donor>)pvr.ViewData.Model).ToList();
             List<Donor> steve = db.Donors.Where(x => x.FirstName == "Steve").ToList();
-            
             Assert.AreEqual(steve.Count(), returnedModel.Count());
-            Assert.AreEqual(steve[0].FirstName, returnedModel[0].FirstName);
+            Assert.IsTrue(steve[0].isEqualTo(returnedModel[0]));
         }
 
         [TestMethod]
         //Tests requesting a donor by name where all donors with the same name are returned.
         public void TestViewMultipleDonors()
         {
-            HomeController hc = new HomeController();
-            FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me Squidward");
-            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
-            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
-
+            DonorsController dc = new DonorsController();
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("donor-search", new List<String> { "name", "Squidward" });
+            parameters.Add("email-address", "");
+            parameters.Add("name", "Squidward");
+            parameters.Add("phone-number", "");
+            PartialViewResult pvr = (PartialViewResult)dc.FetchDonor(parameters);
+            List<Donor> returnedModel = ((List<Donor>)pvr.ViewData.Model).ToList();
             List<Donor> squids = db.Donors.Where(x => x.FirstName == "Squidward").ToList();
-
             Assert.AreEqual(squids.Count(), returnedModel.Count());
-            Assert.AreEqual(squids[0].FirstName, returnedModel[0].FirstName);
+            Assert.IsTrue(squids[0].isEqualTo(returnedModel[0]));
+            Assert.IsTrue(squids[1].isEqualTo(returnedModel[1]));
         }
 
         [TestMethod]
         //Tests searching for a donor by phone number.
         public void TestViewByPhoneNumber()
         {
-            HomeController hc = new HomeController();
-            FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me 555-555-5555");
-            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
-            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
-
-            List<Donor> phone = db.Donors.Where(x => x.PhoneNumber == "555-555-5555").ToList();
-
-            Assert.AreEqual(phone.Count(), returnedModel.Count());
-            Assert.AreEqual(phone[0].PhoneNumber, returnedModel[0].PhoneNumber);
+            DonorsController dc = new DonorsController();
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("donor-search", new List<String> { "phone-number", "555-555-5555" });
+            parameters.Add("email-address", "");
+            parameters.Add("name", "");
+            parameters.Add("phone-number", "555-555-5555");
+            PartialViewResult pvr = (PartialViewResult)dc.FetchDonor(parameters);
+            List<Donor> returnedModel = ((List<Donor>)pvr.ViewData.Model).ToList();
+            List<Donor> steve = db.Donors.Where(x => x.PhoneNumber == "555-555-5555").ToList();
+            Assert.AreEqual(steve.Count(), returnedModel.Count());
+            Assert.IsTrue(steve[0].isEqualTo(returnedModel[0]));
         }
 
         [TestMethod]
         //Tests searching for a donor by Email.
         public void TestViewByEmail()
         {
-            HomeController hc = new HomeController();
-            FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me steve@stevemail.com");
-            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
-            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
-
+            DonorsController dc = new DonorsController();
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("donor-search", new List<String> { "email-address", "steve@stevemail.com"});
+            parameters.Add("email-address" ,"steve@stevemail.com");
+            parameters.Add("name", "");
+            parameters.Add("phone-number", "");
+            PartialViewResult pvr = (PartialViewResult)dc.FetchDonor(parameters);
+            List<Donor> returnedModel = ((List<Donor>)pvr.ViewData.Model).ToList();
             List<Donor> steve = db.Donors.Where(x => x.Email == "steve@stevemail.com").ToList();
-            
             Assert.AreEqual(steve.Count(), returnedModel.Count());
-            Assert.AreEqual(steve[0].FirstName, returnedModel[0].FirstName);
+            Assert.IsTrue(steve[0].isEqualTo(returnedModel[0]));
         }
 
         [TestMethod]
         //Tests a case where a donor with a duplicate first name is searched for with their unique last name and only one proper donor is returned
         public void TestViewSpecificDonorWithDuplicateName()
         {
-            HomeController hc = new HomeController();
-            FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me Squidward Tentacles");
-            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
-            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
-
+            DonorsController dc = new DonorsController();
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("donor-search", new List<String> { "name", "Squidward Tentacles" });
+            parameters.Add("email-address", "");
+            parameters.Add("name", "Squidward Tentacles");
+            parameters.Add("phone-number", "");
+            PartialViewResult pvr = (PartialViewResult)dc.FetchDonor(parameters);
+            List<Donor> returnedModel = ((List<Donor>)pvr.ViewData.Model).ToList();
             List<Donor> squidward = db.Donors.Where(x => x.FirstName == "Squidward" && x.LastName == "Tentacles").ToList();
-
             Assert.AreEqual(squidward.Count(), 1);
-            Assert.AreEqual(squidward[0].FirstName, returnedModel[0].FirstName);
-            Assert.AreEqual(squidward[0].LastName, returnedModel[0].LastName);
+            Assert.IsTrue(squidward[0].isEqualTo(returnedModel[0]));
         }
 
         [TestMethod]
         //Tests searching for a specific donor by lowercase
         public void TestViewSpecificDonorsByLowerCase()
         {
-            HomeController hc = new HomeController();
-            FormCollection fc = new FormCollection();
-            fc.Add("mainInput", "Show me squidward");
-            PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
-            var returnedModel = ((IList<Donor>)returnedView.ViewData.Model).ToList();
-
+            DonorsController dc = new DonorsController();
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("donor-search", new List<String> { "name", "squidward" });
+            parameters.Add("email-address", "");
+            parameters.Add("name", "squidward");
+            parameters.Add("phone-number", "");
+            PartialViewResult pvr = (PartialViewResult)dc.FetchDonor(parameters);
+            List<Donor> returnedModel = ((List<Donor>)pvr.ViewData.Model).ToList();
             List<Donor> squids = db.Donors.Where(x => x.FirstName == "Squidward").ToList();
-
             Assert.AreEqual(squids.Count(), returnedModel.Count());
             Assert.AreEqual(squids[0].FirstName, returnedModel[0].FirstName);
         }
@@ -140,8 +146,8 @@ namespace DMSLite.Tests.Controllers
             FormCollection fc = new FormCollection();
             fc.Add("mainInput", "Order a pizza. This is not a real command.");
             PartialViewResult returnedView = (PartialViewResult)hc.SendInput(fc);
-            var returnedModel = returnedView.ViewData.Model;
-            Assert.IsTrue(returnedModel.Equals("no command found"));
+            string returnedSpeech = ((ResponseModel)returnedView.ViewData.Model).Speech ;
+            Assert.IsTrue(returnedSpeech.Equals("It seems we ran into an error: No command found."));
         }
 
         [TestMethod]
@@ -175,7 +181,7 @@ namespace DMSLite.Tests.Controllers
                 Email = "email_TestAddNewValidDonor",
                 PhoneNumber = "111-111-1111",
             };
-            ContentResult coReturned = (ContentResult)dc.Add(d);
+            ActionResult arReturned = dc.Add(d);
             try
             {
                 dc.Remove(d);
@@ -185,7 +191,7 @@ namespace DMSLite.Tests.Controllers
                 Assert.Fail();
             }
                 //TEMPORARY
-                Assert.IsTrue(coReturned.Content.ToString().Equals("Thanks"));
+                Assert.IsTrue(arReturned.ToString().Equals("Thanks"));
         }
 
         [TestMethod]
