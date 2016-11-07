b396b6f70dda2d1d2aca1907737c6e4e1d03e461
Disva <dravirdisva@gmail.com>
2016-11-07 16:37:19 -0500

    Added in bath controller. Related to Issue #55.

---
+++ DMSLite/Controllers/BatchController.cs
@@ -0,0 +1,35 @@
+﻿using DMSLite.DataContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMSLite.Controllers
+{
+    public class BatchController : Controller
+    {
+        private OrganizationDb db = new OrganizationDb();
+
+        #region Fetch
+        #endregion
+
+        #region Modify
+        #endregion
+
+        #region Add
+        public ActionResult AddMenu(Dictionary<string, object> parameters)
+        {
+            return PartialView("~/Views/Batches/_Add.cshtml", parameters); //view must be made
+        }
+        #endregion
+
+        #region MadeByMs
+        // GET: Batch
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
