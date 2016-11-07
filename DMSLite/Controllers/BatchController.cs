d867bc188282e8e7c51f9379753c42c999251a09
Disva <dravirdisva@gmail.com>
2016-11-07 16:51:49 -0500

    Added in add and addform methods in batchcontroller. _add, _addform, and _addsuccess views will need to be added. Related to issue #55.

--- DMSLite/Controllers/BatchController.cs
+++ DMSLite/Controllers/BatchController.cs
@@ -1,4 +1,5 @@
 ﻿using DMSLite.DataContexts;
+using DMSLite.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,26 @@ namespace DMSLite.Controllers
         public ActionResult AddMenu(Dictionary<string, object> parameters)
         {
             return PartialView("~/Views/Batches/_Add.cshtml", parameters); //view must be made
+        }
+
+        public ActionResult AddForm(Dictionary<string, object> parameters)
+        {
+            Batch newBatch = new Batch();
+            return PartialView("~/Views/Batch/_AddForm.cshtml", newBatch);
+        }
+
+        // TODO: Anti-forgery
+        public ActionResult Add(Batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Batches.Add(batch);
+                db.SaveChanges();
+                return PartialView("~/Views/Batch/_AddSuccess.cshtml", batch);
+            }
+
+            //an invalid submission shall return the form with some validation error messages.
+            return PartialView("~/Views/Batch/_AddForm.cshtml", batch);
         }
         #endregion
 
