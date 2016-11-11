02edacf49b804915378059d11bf4d5ee807ee6e3
Jacob Desrochers <Jabobstev@users.noreply.github.com>
2016-11-11 16:59:16 -0500

    Revert "Issue76 command refactor"

--- DMSLite/Commands/Dispatcher.cs
+++ DMSLite/Commands/Dispatcher.cs
@@ -6,17 +6,14 @@ using ApiAiSDK;
 using System.Web.Mvc;
 using DMSLite.Controllers;
 using DMSLite.Models;
-using Newtonsoft.Json;
-using System.IO;
-using Newtonsoft.Json.Linq;
 
 namespace DMSLite.Commands
-{
+{ 
 
     public class Dispatcher
     {
         private const string apiaikey = "9cc984ef80ef4502baa2de299ce11bbc"; //Client token used
-        private const string CommandsLocation = "Commands.json";
+        private const string CommandsLocation = ".Commands.";
 
         private static ApiAi apiAi;
 
@@ -53,21 +50,29 @@ namespace DMSLite.Commands
 
             Console.WriteLine(response.Result.Fulfillment.Speech);
 
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Commands\", CommandsLocation);
-            StreamReader r = new StreamReader(path);
+            // Get Client's Project Name
+            string project = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
 
-            string json = r.ReadToEnd();
-            var data = JsonConvert.DeserializeObject<Dictionary<string, Tuple<string, string>>>(json);
+            // Taken from API.ai returned JSON object
+            string action = response.Result.Action;
 
+            //Join strings to create classLocation
+            string classLocation = project + ".Commands." + action;
+            
+            //Find the class as a Type
+            Type commandType = Type.GetType(classLocation);
             try
             {
                 //Check if null, if null return message to the UI
-                if (data[response.Result.Action] == null) throw new Exception("No command found.");
+                if (commandType == null) throw new Exception("No command found.");
+            
+                //Cast and execute the command
+                ICommand command = Activator.CreateInstance(commandType) as ICommand;
 
                 ResponseModel responseModel = new ResponseModel()
                 {
                     Speech = response.Result.Fulfillment.Speech,
-                    Instructions = data[response.Result.Action],
+                    Instructions = command.Execute(),
                     Parameters = response.Result.Parameters
                 };
 
@@ -81,7 +86,7 @@ namespace DMSLite.Commands
                 {
                     Speech = "It seems we ran into an error: " + e.Message,
                     Instructions = null,
-                    Parameters = null
+                    Parameters =  null
                 };
 
                 return responseModel;
