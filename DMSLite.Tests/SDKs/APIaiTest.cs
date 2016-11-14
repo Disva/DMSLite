e734ec9a871bc6714473ec7c34622055dd059bb6
pmiri <pmiriello@gmail.com>
2016-11-14 15:38:13 -0500

    #94 Created APIaiTest Test class
    Test class involved all currently closed stories that involve API.ai,
    this also sets the standard for future stories to be closed.

---
+++ DMSLite.Tests/SDKs/APIaiTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApiAiSDK;
+using ApiAiSDK.Model;
+
+namespace DMSLite.Tests.SDKs
+{
+    [TestClass]
+    public class APIaiTest
+    {
+        /**
+         * One must make a test method per Intent
+         * It would also be beneficial to include cases where API.ai *should* fail
+         * Test for returned Action and Parameters.
+         * Include varied input to test for robustness
+         **/
+
+        private const string apiaikey = "9cc984ef80ef4502baa2de299ce11bbc"; //Client token used
+        private static ApiAi apiAi = new ApiAi(new AIConfiguration(apiaikey, SupportedLanguage.English));
+        private Random rand = new Random();
+
+        [TestMethod]
+        public void TestAddBatch()
+        {
+        }
+
+        [TestMethod]
+        public void TestAddDonor()
+        {
+        }
+
+        [TestMethod]
+        public void TestAddDonation()
+        {
+        }
+
+        [TestMethod]
+        public void TestModifyDonor()
+        {
+        }
+
+        [TestMethod]
+        public void TestViewDonor()
+        {
+            string name = "john smith";
+            var response = apiAi.TextRequest("show me " + name);
+            Assert.AreEqual(response.Result.Action, "ViewDonors");
+            Assert.AreEqual(response.Result.Parameters["name"].ToString(), name, true);
+        }
+
+        [TestMethod]
+        public void TestViewDonors()
+        {
+
+        }
+
+        //Sends a randomly selected NL request to API.ai
+        private AIResponse RandomTextInput(string[] inputs)
+        {
+            return apiAi.TextRequest(inputs[rand.Next(0, inputs.Length)]);
+        }
+    }
+}
