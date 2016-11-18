57030c1938871816b11388fb17580da4b4593661
Tyler Ramsay <starvator.ty@gmail.com>
2016-11-18 14:17:36 -0500

    Modified the controller to point to donations #56

--- DMSLite/Controllers/DonationController.cs
+++ DMSLite/Controllers/DonationController.cs
@@ -21,29 +21,31 @@ namespace DMSLite.Controllers
         #region Add
         public ActionResult AddMenu(Dictionary<string, object> parameters)
         {
-            return PartialView("~/Views/Batch/_Add.cshtml", parameters);
+            return PartialView("~/Views/Donation/_Add.cshtml", parameters);
         }
 
         public ActionResult AddForm(Dictionary<string, object> parameters)
         {
-            Batch newBatch = new Batch();
-            if (parameters.ContainsKey("title"))
-                newBatch.Title = parameters["title"].ToString();
-            return PartialView("~/Views/Batch/_AddForm.cshtml", newBatch);
+            Donation newDonation = new Donation();
+           /* if (parameters.ContainsKey("title"))
+                newDonation.Title = parameters["title"].ToString();*/
+            return PartialView("~/Views/Donation/_AddForm.cshtml", newDonation);
         }
 
         // TODO: Anti-forgery
-        public ActionResult Add(Batch batch)
+        public ActionResult Add(Donation donation)
         {
+            //TODO: add to the db
+            /*
             batch.CreateDate = DateTime.Now;
             if (ModelState.IsValid)
             {
                 db.Add(batch);
-                return PartialView("~/Views/Batch/_AddSuccess.cshtml", batch);
+                return PartialView("~/Views/Donation/_AddSuccess.cshtml", batch);
             }
-
+            */
             //an invalid submission shall return the form with some validation error messages.
-            return PartialView("~/Views/Batch/_AddForm.cshtml", batch);
+            return PartialView("~/Views/Donation/_AddForm.cshtml", donation);
         }
         #endregion
 
