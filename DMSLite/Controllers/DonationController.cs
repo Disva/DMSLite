c0c45f2e320b18722ad4e7a1e156b9096f4976f1
Tyler Ramsay <starvator.ty@gmail.com>
2016-11-18 13:53:56 -0500

    Initial commit #56 with structure for donations

---
+++ DMSLite/Controllers/DonationController.cs
@@ -0,0 +1,58 @@
+﻿using DMSLite.DataContexts;
+using DMSLite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMSLite.Controllers
+{
+    public class DonationController : Controller
+    {
+        private OrganizationDb db = new OrganizationDb();
+
+        #region Fetch
+        #endregion
+
+        #region Modify
+        #endregion
+
+        #region Add
+        public ActionResult AddMenu(Dictionary<string, object> parameters)
+        {
+            return PartialView("~/Views/Batch/_Add.cshtml", parameters);
+        }
+
+        public ActionResult AddForm(Dictionary<string, object> parameters)
+        {
+            Batch newBatch = new Batch();
+            if (parameters.ContainsKey("title"))
+                newBatch.Title = parameters["title"].ToString();
+            return PartialView("~/Views/Batch/_AddForm.cshtml", newBatch);
+        }
+
+        // TODO: Anti-forgery
+        public ActionResult Add(Batch batch)
+        {
+            batch.CreateDate = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                db.Add(batch);
+                return PartialView("~/Views/Batch/_AddSuccess.cshtml", batch);
+            }
+
+            //an invalid submission shall return the form with some validation error messages.
+            return PartialView("~/Views/Batch/_AddForm.cshtml", batch);
+        }
+        #endregion
+
+        #region MadeByMs
+        // GET: Batch
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
