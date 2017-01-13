3c1140d45f784294750ef08782693abb07efc94e
Denis <deniskefallinos94@gmail.com>
2017-01-13 16:47:08 -0500

    Unit test for #58
    TestFetchBatchByTitle

--- DMSLite.Tests/Controllers/BatchControllerTest.cs
+++ DMSLite.Tests/Controllers/BatchControllerTest.cs
@@ -132,6 +132,45 @@ namespace DMSLite.Tests.Controllers
         }
 
         [TestMethod]
+        //Tests fetching a batch by dates before and after
+        public void TestFetchBatchByDate()
+        {
+            //adds a new testing batch to the db
+            BatchController bc = new BatchController(db);
+            Batch b = new Batch()
+            {
+                Title = "TestFetchBatchByDate",
+            };
+            b = (Batch)(((PartialViewResult)(bc.Add(b))).Model);
+            List<Batch> dbBatches = db.Batches.Where(x => x.Id == b.Id).ToList();
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            //searches for that open batch by date before
+            parameters.Add("title", "TestFetchBatchByDate");
+            parameters.Add("date", b.CreateDate.AddDays(5).ToString("yyyy-MM-dd"));
+            parameters.Add("datetype", "before");
+            parameters.Add("type", "open");
+            parameters.Add("posttype", "opened");
+            List<Batch> testBatches = bc.FindBatches(parameters);
+            Assert.AreEqual(dbBatches.Count, testBatches.Count);
+            Assert.AreEqual(dbBatches.First().Title, testBatches.First().Title);
+            //close batch
+            bc.PostBatch(b.Id);
+            //searches for that closed batch by date after
+            parameters = new Dictionary<string, object>();
+            parameters.Add("title", "TestFetchBatchByDate");
+            parameters.Add("date", b.CreateDate.AddDays(-5).ToString("yyyy-MM-dd"));
+            parameters.Add("datetype", "after");
+            parameters.Add("type", "close");
+            parameters.Add("posttype", "closed");
+            testBatches = bc.FindBatches(parameters);
+            dbBatches = db.Batches.Where(x => x.Id == b.Id).ToList();
+            Assert.AreEqual(dbBatches.Count, testBatches.Count);
+            Assert.AreEqual(dbBatches.First().Title, testBatches.First().Title);
+            //remove testing batch
+            bc.Remove(b);
+        }
+
+        [TestMethod]
         //Tests that viewing a fetced batch either returns a view of the batches donations or an error message if it has none
         public void TestViewFetchedBatch()
         {
