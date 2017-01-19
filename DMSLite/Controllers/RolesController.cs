8b24918e0c32d44c53ea7f9b871cf065c230c8ad
Netopya <netopya@netopyaplanet.com>
2017-01-19 17:35:19 -0500

    Added priliminary role manager
    -For Issue #129
    -Added a roles controller to add and view roles

---
+++ DMSLite/Controllers/RolesController.cs
@@ -0,0 +1,56 @@
+﻿using DMSLite.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMSLite.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RolesController()
+        {
+            context = new ApplicationDbContext();
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+        }
+
+        public ActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: /Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Roles/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                context.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
