71a5e0ed8c20984108ec9154097bfcd1a7d10e87
dsvoid <dsvoid@gmail.com>
2017-02-02 17:23:30 -0500

    #53 Tested receipt generation with donor object

--- DMSLite/Controllers/ReceiptController.cs
+++ DMSLite/Controllers/ReceiptController.cs
@@ -9,11 +9,25 @@ using PdfSharp.Pdf.IO;
 using PdfSharp.Drawing;
 using DMSLite.Entities;
 using System.IO;
+using DMSLite.DataContexts;
+using DMSLite.Controllers;
 
 namespace DMSLite.Controllers
 {
     public class ReceiptController : Controller
     {
+        private OrganizationDb db = new OrganizationDb();
+
+        public ReceiptController()
+        {
+            db = new OrganizationDb();
+        }
+
+        public ReceiptController(OrganizationDb db)
+        {
+            this.db = db;
+        }
+
         // GET: Receipt
         public ActionResult Index()
         {
@@ -24,7 +38,6 @@ namespace DMSLite.Controllers
         {
             // we are using this MemoryStream to be able to access the PDF as an ActionResult.
             // source: https://stackoverflow.com/questions/15121876/how-do-i-display-a-pdf-using-pdfsharp-in-asp-net-mvc#15145131
-            //lol
             using (MemoryStream stream = new MemoryStream())
             { 
                 // Create a new PDF document
@@ -44,6 +57,40 @@ namespace DMSLite.Controllers
                 gfx.DrawString("Hello, World!", font, XBrushes.Black,
                     new XRect(0, 0, page.Width, page.Height),
                     XStringFormats.Center);
+
+                // Save the document...
+                document.Save(stream, false);
+
+                return File(stream.ToArray(), "application/pdf");
+            }
+        }
+
+        public ActionResult TestWithDonor()
+        {
+            Donor donor = db.Donors.First();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                // Create a new PDF document
+                PdfDocument document = new PdfDocument();
+                document.Info.Title = "Created with PDFsharp";
+
+                // Create an empty page
+                PdfPage page = document.AddPage();
+
+                // Get an XGraphics object for drawing
+                XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                // Create a font
+                XFont font = new XFont("Verdana", 12, XFontStyle.Regular);
+
+                string nameField = donor.FirstName + " " + donor.LastName;
+                string addressField = donor.Address;
+                string full = nameField + "\n" + addressField + "\n";
+
+                // Draw the text
+                gfx.DrawString(full, font, XBrushes.Black,
+                    new XRect(100, 100, page.Width - 100, page.Height - 100),
+                    XStringFormats.TopLeft);
 
                 // Save the document...
                 document.Save(stream, false);
