0860e786c23ff25d0bef11a1829b160752c3d380
dsvoid <dsvoid@gmail.com>
2017-02-06 15:47:56 -0500

    #53 Receipts now contain batch and donation data per donor

--- DMSLite/Controllers/ReceiptController.cs
+++ DMSLite/Controllers/ReceiptController.cs
@@ -11,6 +11,7 @@ using DMSLite.Entities;
 using System.IO;
 using DMSLite.DataContexts;
 using DMSLite.Controllers;
+using PdfSharp.Drawing.Layout;
 
 namespace DMSLite.Controllers
 {
@@ -68,30 +69,38 @@ namespace DMSLite.Controllers
         public ActionResult TestWithDonor()
         {
             Donor donor = db.Donors.First();
+            List<Donation> donations = db.Donations.Where(x => x.DonationDonor_Id.Equals(donor.Id)).ToList();
             using (MemoryStream stream = new MemoryStream())
             {
+                // Create output string
+                string full = donor.FirstName + " " + donor.LastName + "\n";
+                full += donor.Address + "\n";
+                full += "Donations:\n";
+                foreach (var donation in donations)
+                {
+                    donation.DonationBatch = db.Batches.Where(x => x.Id.Equals(donation.DonationBatch_Id)).First();
+                    if (donation.Value > 0)
+                        full += "$" + donation.Value + " to " + donation.DonationBatch.Title
+                             + " on " + donation.DonationBatch.CreateDate + "\n";
+                    if (donation.ObjectDescription != "")
+                        full += "Description: " + donation.ObjectDescription + "\n";
+                }
                 // Create a new PDF document
                 PdfDocument document = new PdfDocument();
                 document.Info.Title = "Created with PDFsharp";
-
                 // Create an empty page
                 PdfPage page = document.AddPage();
-
                 // Get an XGraphics object for drawing
                 XGraphics gfx = XGraphics.FromPdfPage(page);
-
                 // Create a font
                 XFont font = new XFont("Verdana", 12, XFontStyle.Regular);
-
-                string nameField = donor.FirstName + " " + donor.LastName;
-                string addressField = donor.Address;
-                string full = nameField + "\n" + addressField + "\n";
-
-                // Draw the text
-                gfx.DrawString(full, font, XBrushes.Black,
-                    new XRect(100, 100, page.Width - 100, page.Height - 100),
-                    XStringFormats.TopLeft);
-
+                // Create a text formatter to make sure newlines work on the rendered string
+                XTextFormatter tf = new XTextFormatter(gfx);
+                // Draw a rectangle to contain the text
+                XRect rect = new XRect(100, 100, page.Width - 100, page.Height - 100);
+                gfx.DrawRectangle(XBrushes.White, rect);
+                // Draw the text using the text formatter
+                tf.DrawString(full, font, XBrushes.Black, rect, XStringFormats.TopLeft);
                 // Save the document...
                 document.Save(stream, false);
 
