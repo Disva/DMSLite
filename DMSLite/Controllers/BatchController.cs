a0885517dbbfc1b42aed568dd9cd237d71b39189
pmiri <pmiriello@gmail.com>
2017-03-02 15:29:09 -0500

    #262 Logging form submissions with JSON serialized objects

--- DMSLite/Controllers/BatchController.cs
+++ DMSLite/Controllers/BatchController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace DMSLite.Controllers
 {
+    using Newtonsoft.Json;
     using DateRange = Tuple<DateTime, DateTime>;
 
     [Authorize]
@@ -331,6 +332,7 @@ namespace DMSLite.Controllers
                 if (ModelState.IsValid)
                 {
                     db.Add(batch);
+                    Helpers.Log.WriteLog(Helpers.Log.LogType.ParamsSubmitted, JsonConvert.SerializeObject(batch));
                     return PartialView("~/Views/Batch/_AddSuccess.cshtml", batch);
                 }
             }
