a8848c253b83324fa53a3b3eb6aa0e18aac5ddb8
dsvoid <dsvoid@gmail.com>
2017-03-02 15:59:19 -0500

    #53 Added Receipt entity and receipt id to Donor

---
+++ DMALite.Entities/Receipt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMSLite.Entities
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime IssueDate { get; set; }
+    }
+}
