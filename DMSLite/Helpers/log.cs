f310866869b713f657fea9a096cc6c799a6d2da3
Tyler Ramsay <starvator.ty@gmail.com>
2017-03-04 12:58:12 -0500

    #42 type for LogType Task

--- DMSLite/Helpers/log.cs
+++ DMSLite/Helpers/log.cs
@@ -28,7 +28,7 @@ namespace DMSLite.Helpers
             public static LogType ParamsFound { get { return new LogType("->>"); } }
             public static LogType ParamsSubmitted { get { return new LogType("<<-"); } }
             public static LogType Bug { get { return new LogType("!"); } }
-            public static LogType Task { get { return new LogType("!"); } }
+            public static LogType Task { get { return new LogType("#"); } }
             public static LogType Test { get { return new LogType(" T "); } }
         }
 
