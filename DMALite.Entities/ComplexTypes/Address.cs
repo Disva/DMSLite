beb9ceff95f4fab2b03cb460fa853efc719730eb
Netopya <netopya@netopyaplanet.com>
2017-03-06 16:24:57 -0500

    #280 Added fields to entities
    -Added address and other organization information to appropriates
    entities

---
+++ DMALite.Entities/ComplexTypes/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMSLite.Entities.ComplexTypes
+{
+    [ComplexType]
+    public class Address
+    {
+        [StringLength(255)]
+        public string AddressLineOne { get; set; }
+
+        [StringLength(255)]
+        public string AddressLineTwo { get; set; }
+
+        [StringLength(255)]
+        public string City { get; set; }
+
+        [StringLength(255)]
+        public string Province { get; set; }
+
+        [StringLength(255)]
+        public string Country { get; set; }
+
+        [StringLength(255)]
+        public string PostalCode { get; set; }
+    }
+}
