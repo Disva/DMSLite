62f6e3d7d274a5be4baf105aaa64686c7cc82cf6
Michael B <netopya@netopyaplanet.com>
2017-03-10 21:21:17 -0500

    Started work on viewmodel

---
+++ DMSLite/Models/DonationFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DMSLite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMSLite.Models
+{
+    public class DonationFormViewModel
+    {
+        public Donation donation {get; set;}
+
+        public int donationDonor { get; set; }
+        public int donationBatch { get; set; }
+
+        public List<Batch> batches { get; set; }
+        public List<Donor> donors { get; set; }
+    }
+}
