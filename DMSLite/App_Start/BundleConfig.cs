3d7dc189b86eb26a2b0920734b15b439642eadfd
pmiri <pmiriello@gmail.com>
2017-03-13 16:33:34 -0400

    #279 replaced script tags with bundle references
    using cs Script Render

--- DMSLite/App_Start/BundleConfig.cs
+++ DMSLite/App_Start/BundleConfig.cs
@@ -8,8 +8,14 @@ namespace DMSLite
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));*/
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-unobtrusive").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/notify").Include(
+                        "~/Scripts/notify.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -30,6 +36,13 @@ namespace DMSLite
 
             bundles.Add(new ScriptBundle("~/bundles/select2").Include(
                 "~/Scripts/select2.js"));
+
+            //Custom Scripts
+            bundles.Add(new ScriptBundle("~/bundles/home").Include(
+                "~/Scripts/Custom/homeJavascript.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/suggestionBar").Include(
+                "~/Scripts/Custom/suggestionbar.js"));
         }
     }
 }
