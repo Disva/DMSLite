1691230fc9c9c79801b5cea2d9308645f0b6c146
Netopya <netopya@netopyaplanet.com>
2017-03-27 17:24:05 -0400

    #276 Initial CSV support
    -Added controller and a simple view

---
+++ DMSLite/Controllers/CSVController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CsvHelper;
+using System.IO;
+using DMSLite.DataContexts;
+using DMSLite.Entities;
+
+namespace DMSLite.Controllers
+{
+    public class CSVController : Controller
+    {
+        private OrganizationDb db = new OrganizationDb(); 
+
+        // GET: CSV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileStreamResult ExportAllDonors()
+        {
+            var result = WriteCsvToMemory(db.Donors);
+            var memoryStream = new MemoryStream(result);
+            return new FileStreamResult(memoryStream, "text/csv") { FileDownloadName = "export.csv" };
+        }
+
+        public ActionResult ImportDonors(FormCollection form, HttpPostedFileBase donorupload)
+        {
+            var csv = new CsvReader(new StreamReader(donorupload.InputStream));
+            var records = csv.GetRecords<Donor>();
+            db.Add(records.First());
+            return null;
+        }
+
+        public FileStreamResult ExportAllDonations()
+        {
+            var result = WriteCsvToMemory(db.Donations);
+            var memoryStream = new MemoryStream(result);
+            return new FileStreamResult(memoryStream, "text/csv") { FileDownloadName = "export.csv" };
+        }
+
+        public byte[] WriteCsvToMemory<T>(IEnumerable<T> records)
+        {
+            using (var memoryStream = new MemoryStream())
+            using (var streamWriter = new StreamWriter(memoryStream))
+            using (var csvWriter = new CsvWriter(streamWriter))
+            {
+                csvWriter.WriteRecords(records);
+                streamWriter.Flush();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
